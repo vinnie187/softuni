7732a585c17bcf933c21bc90f846489ad68db570
vinnie187 <venelin.velev@outlook.com>
2019-11-23 14:30:49 +0200

    .

--- Programming basics with JS/Simple Operations and Calculations/Hello.js
+++ Programming basics with JS/Simple Operations and Calculations/Hello.js
@@ -1,6 +1,6 @@
 function hello(input) {
     let name = input.shift();
-    let age = input.shift();
+    let age = Number(input.shift());
     console.log(`Hello,my name is ${name} and I am ${age} years old!`);
 }
 
---
+++ Programming basics with JS/Simple Operations and Calculations/solve.js
@@ -0,0 +1,11 @@
+function solve() {
+    let firstName = input.shift();
+    let lastName = input.shift();
+    let age = Number(input.shift());
+    let town = input.shift();
+    console.log("You are" + firstName + lastName );
+    
+
+}
+
+solve(["Pesho","Peshov","19","Shumen"]);
